2a308a18f1014ba8708800f0744225439eff3862
HamzaLghali <vanhamzalghali@gmail.com>
2023-05-16 20:35:33 +0100

    first

---
+++ src/test/java/com/example/stuber/StuberBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stuber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StuberBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
