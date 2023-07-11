314cfc9152970943717b1a508d1bbac8bdbde7df
HamzaLghali <vanhamzalghali@gmail.com>
2023-07-11 11:09:08 +0100

    auth4

---
+++ src/main/java/com/example/stuber/auth/JwtService.java
@@ -0,0 +1,76 @@
+package com.example.stuber.auth;
+
+
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.security.SecureRandom;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+@Service
+public class JwtService {
+
+	private static final String SIGNING_KEY = "t4GBqsSPPKKAK8VgDQQ0MxRyxpfOElTN";
+
+	public String extractUsername(String token) {
+		return extractClaims(token, Claims::getSubject);
+	}
+
+	public <T> T extractClaims(String token, Function<Claims, T> claimsResolver) {
+		final Claims claims = extractAllClaims(token);
+		return claimsResolver.apply(claims);
+	}
+
+	public String generateToken(UserDetails userDetails) {
+		return generateToken(new HashMap<>(), userDetails);
+	}
+
+	public boolean isTokenValid(String token, UserDetails userDetails) {
+		final String username = extractUsername(token);
+		return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+	}
+
+	private boolean isTokenExpired(String token) {
+		return extractExpiration(token).before(new Date());
+	}
+
+	private Date extractExpiration(String token) {
+		return extractClaims(token, Claims::getExpiration);
+	}
+
+	public String generateToken(Map<String, Object> extarctClaims, UserDetails userDetails) {
+
+		return Jwts
+				.builder()
+				.setSubject(userDetails.getUsername())
+				.setClaims(extarctClaims)
+				.setIssuedAt(new Date(System.currentTimeMillis()))
+				.setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24 * 7))
+				.signWith(getSigningKey(), SignatureAlgorithm.HS256)
+				.compact();
+	}
+
+	public Claims extractAllClaims(String token) {
+		return Jwts
+				.parserBuilder()
+				.setSigningKey(getSigningKey())
+				.build()
+				.parseClaimsJws(token)
+				.getBody();
+	}
+
+	private Key getSigningKey() {
+		byte[] keyBytes = Decoders.BASE64.decode(SIGNING_KEY);
+		return Keys.hmacShaKeyFor(keyBytes);
+	}
+}
